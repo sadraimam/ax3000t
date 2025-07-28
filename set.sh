742607d1820644d71d83decbe97cda3928bed8dd
sadraimam <sadra.imam@gmail.com>
2025-07-28 20:10:14 +0330

    Update set.sh

--- set.sh
+++ set.sh
@@ -189,13 +189,11 @@ uci set wireless.radio0.cell_density='0'
 uci set wireless.default_radio0.encryption='sae-mixed'
 uci set wireless.default_radio0.key='123456789'
 uci set wireless.default_radio0.ocv='0'
+uci set wireless.radio0.disabled='0'
 uci set wireless.radio1.cell_density='0'
 uci set wireless.default_radio1.encryption='sae-mixed'
 uci set wireless.default_radio1.key='123456789'
 uci set wireless.default_radio1.ocv='0'
-uci commit wireless
-wifi reload
-uci set wireless.radio0.disabled='0'
 uci set wireless.radio1.disabled='0'
 uci commit wireless
 wifi reload
