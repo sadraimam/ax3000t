58872c62c99926956de704d88bae7df1d80cb668
sadraimam <sadra.imam@gmail.com>
2025-07-29 15:37:07 +0330

    Update set.sh

--- set.sh
+++ set.sh
@@ -175,6 +175,7 @@ uci delete passwall2.Netflix
 uci delete passwall2.OpenAI
 uci delete passwall2.China
 uci delete passwall2.QUIC
+uci delete passwall2.Proxy
 uci delete passwall2.@global_subscribe[0].filter_discard_list
 
 uci set passwall2.Direct=shunt_rules
