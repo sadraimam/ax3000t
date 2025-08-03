c229bb827b935d43f304dfc5bc4cb7d392899448
sadraimam <sadra.imam@gmail.com>
2025-08-04 01:55:28 +0330

    Update set.sh

--- set.sh
+++ set.sh
@@ -48,17 +48,20 @@ uci commit network
 echo "${GREEN} Initialized! ${NC}"
 
 # Add Passwall Feeds
-wget -O passwall.pub https://master.dl.sourceforge.net/project/openwrt-passwall-build/passwall.pub
-opkg-key add passwall.pub
->/etc/opkg/customfeeds.conf
-
-read release arch << EOF
-$(. /etc/openwrt_release ; echo ${DISTRIB_RELEASE%.*} $DISTRIB_ARCH)
+#wget -O passwall.pub https://master.dl.sourceforge.net/project/openwrt-passwall-build/passwall.pub
+#opkg-key add passwall.pub
+#>/etc/opkg/customfeeds.conf
+wget -O /tmp/passwall.pub https://master.dl.sourceforge.net/project/openwrt-passwall-build/passwall.pub
+opkg-key add /tmp/passwall.pub
+rm -f /tmp/passwall.pub
+> /etc/opkg/customfeeds.conf
+read release arch <<EOF
+$(. /etc/openwrt_release; echo ${DISTRIB_RELEASE%.*} $DISTRIB_ARCH)
 EOF
 for feed in passwall_luci passwall_packages passwall2; do
   echo "src/gz $feed https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/$feed" >> /etc/opkg/customfeeds.conf
 done
-echo "${GREEN}Feed Updated!${NC}"
+echo -e "${GREEN}Feed Updated!${NC}"
 
 opkg update
 
