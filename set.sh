e9767d62cc84558b2ba06ddfbbdfbeed73e2154a
Insᴓ. <sadra.imam@gmail.com>
2025-08-07 14:01:42 +0330

    Update set.sh

--- set.sh
+++ set.sh
@@ -37,14 +37,6 @@ uci commit
 /sbin/reload_config
 echo -e "${GREEN}System Initialized! ${NC}"
 
-# Force NTP resync
-#uci delete system.ntp.server
-#uci add_list system.ntp.server='ir.pool.ntp.org'
-#uci add_list system.ntp.server='0.openwrt.pool.ntp.org'
-##uci add_list system.ntp.server='1.openwrt.pool.ntp.org'
-#uci commit system
-#/etc/init.d/sysntpd restart
-
 # Force NTP sync (with retry fallback)
 echo -e "${YELLOW}Syncing time with NTP...${NC}"
 ntpd -n -q -p ir.pool.ntp.org || {
