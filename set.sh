1712b9caa4e3142ad9a48fcc025cbbf70143cc77
Insᴓ. <sadra.imam@gmail.com>
2025-08-09 11:14:38 +0330

    Update set.sh

--- set.sh
+++ set.sh
@@ -44,9 +44,14 @@ echo -e "${GREEN}Network Initialized! ${NC}"
 # Initialize Time/Date
 uci set system.@system[0].zonename='Asia/Tehran'
 uci set system.@system[0].timezone='<+0330>-3:30'
+uci delete system.ntp.server
+uci add_list system.ntp.server='ir.pool.ntp.org'
+uci add_list system.ntp.server='0.openwrt.pool.ntp.org'
+uci add_list system.ntp.server='1.openwrt.pool.ntp.org'
 uci commit system
 /etc/init.d/sysntpd restart
 echo -e "${GREEN}Time/Date Initialized! ${NC}"
+
 echo -e "${YELLOW}Syncing time with NTP...${NC}"
 ntpd -n -q -p ir.pool.ntp.org || {
   echo -e "${RED}NTP sync failed! Retrying with global pool...${NC}"
